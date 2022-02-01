6c11a840d9c4b073f9b542a9a4613c033b7ac63a
Prasadrpatil <prasadrpatil25.10@gmail.com>
2022-02-01 14:52:51 +0530

    Navbar Mini Logo Changes

--- frontend/src/app/shared/Navbar.js
+++ frontend/src/app/shared/Navbar.js
@@ -43,7 +43,15 @@ const Navbar = () => {
     <nav className='navbar p-0 fixed-top d-flex flex-row'>
       <div className='navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center'>
         <Link className='navbar-brand brand-logo-mini' to='/'>
-          <img src={require('../../assets/images/logo-mini.svg')} alt='logo' />
+          <div
+            style={{
+              marginLeft: '5px',
+              marginTop: '15px',
+              color: 'white',
+            }}
+          >
+            <h4>B I T S A F E</h4>
+          </div>
         </Link>
       </div>
       <div className='navbar-menu-wrapper flex-grow d-flex align-items-stretch'>
